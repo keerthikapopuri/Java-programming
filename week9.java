616cd6288142d8ed08febdc11820005e914b1ab0
Popuri Keerthika <79918843+keerthikapopuri@users.noreply.github.com>
2022-02-17 14:49:15 +0530

    Add files via upload

---
+++ week9.java
@@ -0,0 +1,23 @@
+class week9{
+public static void def(int index) throws Exception{
+if(index<0)
+{
+ throw new Exception("NegativeArraySizeException");
+}
+else
+{
+System.out.println(index);
+}
+}
+public static void main(String[] args)
+{
+int[] a = new int[5];
+try{
+ def(-3);
+}
+catch(Exception e)
+{
+System.out.println(e);
+}
+}
+}
